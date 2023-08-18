a2a66b99ddb49e84aedcc60c1ef790edd4053381
DuyThang <thangcbq78@gmail.com>
2023-08-18 17:40:24 +0700

    filter and sort

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,54 +1,84 @@
-const Product = require('../models/product')
-const asyncHandler = require('express-async-handler')
-const slugify = require('slugify')
+const { response } = require("express");
+const Product = require("../models/product");
+const asyncHandler = require("express-async-handler");
+const slugify = require("slugify");
 
 const createProduct = asyncHandler(async (req, res) => {
-    if (Object.keys(req.body).length === 0) throw new Error('Missing inputs')
-    if (req.body && req.body.title) req.body.slug = slugify(req.body.title)
-    const newProduct = await Product.create(req.body)
-    return res.status(200).json({
-        success: newProduct ? true : false,
-        createdProduct: newProduct ? newProduct : 'Cannot create new product'
-    })
-})
+  if (Object.keys(req.body).length === 0) throw new Error("Missing inputs");
+  if (req.body && req.body.title) req.body.slug = slugify(req.body.title);
+  const newProduct = await Product.create(req.body);
+  return res.status(200).json({
+    success: newProduct ? true : false,
+    createdProduct: newProduct ? newProduct : "Cannot create new product",
+  });
+});
 const getProduct = asyncHandler(async (req, res) => {
-    const { pid } = req.params
-    const product = await Product.findById(pid)
-    return res.status(200).json({
-        success: product ? true : false,
-        productData: product ? product : 'Cannot get product'
-    })
-})
+  const { pid } = req.params;
+  const product = await Product.findById(pid);
+  return res.status(200).json({
+    success: product ? true : false,
+    productData: product ? product : "Cannot get product",
+  });
+});
 // Filtering, sorting & pagination
 const getProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find()
+  const queries = { ...req.query };
+  // Tach cac truong dac biet ra khoi query
+  const excludeFields = ["limit", "sort", "page", "fields"];
+  excludeFields.forEach(el => delete queries[el]);
+
+  // format lai cac operator cho dung cu phap mongoose
+  let queryString = JSON.stringify(queries);
+ queryString = queryString.replace(/\b(gte|gt|lt|lte)\b/g, (macthedEl) => `$${macthedEl}`)
+  const formatedQueries = JSON.parse(queryString);
+
+  // filtering
+  if (queries?.title)
+    formatedQueries.title = { $regex: queries.title, $options: "i" };
+  let queryCommand = Product.find(formatedQueries);
+
+  // sorting
+    if (req.query.sort){
+        const sortBy = req.query.sort.split(',').join(' ');
+        queryCommand = queryCommand.sort(sortBy);
+    }
+
+  // Excute query
+  // so luong san pham thoa man !== so luong sp tra ve 1 lan goi API
+  queryCommand.exec(async (err, response) => {
+    if (err) throw new Error(err.message);
+    const counts = await Product.find(formatedQueries).countDocuments();
     return res.status(200).json({
-        success: products ? true : false,
-        productDatas: products ? products : 'Cannot get products'
-    })
-})
+      success: response ? true : false,
+      products: response ? response : "Cannot get products",
+      counts
+    });
+  });
+});
 const updateProduct = asyncHandler(async (req, res) => {
-    const { pid } = req.params
-    if (req.body && req.body.title) req.body.slug = slugify(req.body.title)
-    const updatedProduct = await Product.findByIdAndUpdate(pid, req.body, { new: true })
-    return res.status(200).json({
-        success: updatedProduct ? true : false,
-        updatedProduct: updatedProduct ? updatedProduct : 'Cannot update product'
-    })
-})
+  const { pid } = req.params;
+  if (req.body && req.body.title) req.body.slug = slugify(req.body.title);
+  const updatedProduct = await Product.findByIdAndUpdate(pid, req.body, {
+    new: true,
+  });
+  return res.status(200).json({
+    success: updatedProduct ? true : false,
+    updatedProduct: updatedProduct ? updatedProduct : "Cannot update product",
+  });
+});
 const deleteProduct = asyncHandler(async (req, res) => {
-    const { pid } = req.params
-    const deletedProduct = await Product.findByIdAndDelete(pid)
-    return res.status(200).json({
-        success: deletedProduct ? true : false,
-        deletedProduct: deletedProduct ? deletedProduct : 'Cannot delete product'
-    })
-})
+  const { pid } = req.params;
+  const deletedProduct = await Product.findByIdAndDelete(pid);
+  return res.status(200).json({
+    success: deletedProduct ? true : false,
+    deletedProduct: deletedProduct ? deletedProduct : "Cannot delete product",
+  });
+});
 
 module.exports = {
-    createProduct,
-    getProduct,
-    getProducts,
-    updateProduct,
-    deleteProduct
-}
+  createProduct,
+  getProduct,
+  getProducts,
+  updateProduct,
+  deleteProduct,
+};
